c969dc65b4d728addcc6737719fd4342ddc22ac0
jieqiang <jieqqq678@163.com>
2019-03-13 07:04:00 +0800

    修改publish

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,28 @@
-var path=require('path');
+var path = require('path');
+//导入插件
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
-
-
-var config={
-    entry:{
-        main:'./main'
+var config = {
+    entry: {
+        main: './main'
     },
-    output:{
-        path:path.join(__dirname,'./dist'),
-        publicPath:'./dist',
-        filename:'main.js'
-    }
+    output: {
+        path: path.join(__dirname, './dist'),
+        publicPath: '/dist/',
+        filename: 'main.js'
+    },
+    module: {
+        rules: [{
+            test: /\.css$/,
+            use: ExtractTextPlugin.extract({
+                use: 'css-loader',
+                fallback: 'style-loader'
+            })
+        }]
+    },
+    plugins: [
+        new ExtractTextPlugin("main.css")
+    ]
 }
 
-module.exports=config;
+module.exports = config;
