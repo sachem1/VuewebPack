15cb323f7167e1ebb555fb9578dbecb45974c5af
jieqiang <jieqqq678@163.com>
2019-03-13 18:44:47 +0800

    添加测试vue

--- app.vue
+++ app.vue
@@ -1,8 +1,24 @@
 <template>
-  <div>Hello {{name}}</div>
+  <div>
+    <v-title title="Vue 组件化"></v-title>
+    <v-button @click="handleClick">点击按钮</v-button>
+  </div>
 </template>
 <script>
+//导入组件
+import vTitle from "./title.vue";
+import vButton from "./button.vue";
+
 export default {
+  components: {
+    vTitle,
+    vButton
+  },
+  methods: {
+    handleClick(e) {
+      console.log(e);
+    }
+  },
   data() {
     return {
       name: "Vue.js"
@@ -11,10 +27,9 @@ export default {
 };
 </script>
 <style>
-div{
-    color:#f60;
+div {
+  color: #f60;
 }
-
 
 </style>
 
---
+++ button.vue
@@ -0,0 +1,45 @@
+<template>
+  <button @click="handleClick" :style="styles"></button>
+</template>
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "#00cc66"
+    }
+  },
+  computed: {
+    styles() {
+      return {
+        background: this.color
+      };
+    }
+  },
+  methods: {
+    handleClick(e) {
+      this.$emit("click", e);
+    }
+  }
+};
+</script>
+<style>
+button {
+  border: 0;
+  outline: none;
+  color: #fff;
+  padding: 4px 8px;
+  width: 50px;
+  height: 30px;;
+}
+button:active {
+  position: relative;
+  top: 1px;
+  left: 1px;
+}
+
+
+
+
+
+</style>
